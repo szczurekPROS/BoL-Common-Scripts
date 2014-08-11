c6e3cd65e986c606cb99f05e87402c0b5d80da5e
szczurekPROS <szczurekPROS@o2.pl>
2014-08-11 22:20:25 +0200

    Update AISpell.lua

--- AISpell.lua
+++ AISpell.lua
@@ -1,3 +1,29 @@
+local version = "1.0"
+local AUTOUPDATE = false
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/szczurekPROS/Common/master/AISpell.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = LIB_PATH.."AISpell.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+
+local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>AISpell:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, "/szczurekPROS/Common/master/version/AISpell.version")
+	if ServerData then
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+		if ServerVersion then
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available"..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
+
 --[[
 AI lib version 1
 by szczurekPROS
